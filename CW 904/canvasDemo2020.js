2b38d090dc96a73db6b3955666ec35f5188b78d2
Emily Greene <798007@seq.org>
2020-09-11 11:14:00 -0700

    911(3)

--- CW 904/canvasDemo2020.js
+++ CW 904/canvasDemo2020.js
@@ -29,39 +29,37 @@ function Ball(x, y, dx, dy, radius, color){
   var green = Math.floor(Math.random()*255);
   var blue = Math.floor(Math.random()*255);
   this.color = 'rgba('+red+', '+green+', '+blue+')';
-
-  this.run = function(){
-    this.update();
-    this.render();
-  }
-
-  this.update = function(){
-    this.x += this.dx;
-    this.y += this.dy;
-    if(this.x > window.innerWidth || this.x < 0) this.dx = -this.dx;
-    if(this.y > window.innerHight || this.y < 0) this.dy = -this.dy;
-  }
-
-  this.render = function(){
-    //ctx.clearRect(0,0,canvas.width, canvas.height);
-    ctx.strokeStyle = this.color;
-    ctx.fillStyle = this.color;
-    ctx.beginPath();
-    ctx.arc(this.x,this.y,this.radius, Math.PI*2, 0, false);
-    ctx.fill();
-    ctx.stroke();
-  }
 }
 
+//this function moves the ball and keeps it on the screen
+Ball.prototype.update = function(){
+  this.x += this.dx;
+  this.y += this.dy;
+  if(this.x > canvas.width || this.x < 0) this.dx = -this.dx;
+  if(this.y > canvas.height || this.y < 0) this.dy = -this.dy;
+  this.render();
+}
+
+//this function draws the ball
+Ball.prototype.render = function(){
+  ctx.strokeStyle = this.color;
+  ctx.fillStyle = this.color;
+  ctx.beginPath();
+  ctx.arc(this.x,this.y,this.radius, Math.PI*2, 0, false);
+  ctx.fill();
+  ctx.stroke();
+}
+
+//animates every ball object that is loaded into the ball array
 function animate(){
+  ctx.clearRect(0,0,canvas.width, canvas.height);
   for(var i = 0; i < ballArr.length; i++){
-    ballArr[i].run();
+    ballArr[i].update();
   }
   requestAnimationFrame(animate);
 }
 
-
-
+//loads ball objects into an array - the parameter number is the amount of ball objects that get loaded into the array
 function loadBalls(number){
   for(var i=0; i < number; i++){
     var x = Math.random()*window.innerWidth;
