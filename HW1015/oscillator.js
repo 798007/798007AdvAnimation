f76012d70ebee909d316db26472b901b7ab774b6
Emily Greene <798007@seq.org>
2020-10-20 00:21:05 -0700

    1020

---
+++ HW1015/oscillator.js
@@ -0,0 +1,31 @@
+function Oscillator(){
+  this.angle = new JSVector(100, 0);
+  this.velocity = new JSVector(Math.random()*0.05, Math.random()*0.05);
+  this.amplitude = new JSVector(Math.random()*(canvas.width/2), Math.random()*(canvas.height/2))
+
+}
+
+Oscillator.prototype.run = function(){
+  this.oscillate();
+  this.display();
+}
+
+Oscillator.prototype.oscillate = function(){
+  this.angle.add(this.velocity);
+}
+
+Oscillator.prototype.display = function(){
+  let ctx = game.ctx;
+  this.x = Math.sin(this.angle.x)*this.amplitude.x;
+  this.y = Math.sin(this.angle.y)*this.amplitude.y;
+
+  ctx.strokeStyle = 'rgba(0, 0, 0, 1)'
+  ctx.save();
+  ctx.beginPath();
+  ctx.translate(canvas.width/2, canvas.height/2);
+  ctx.lineTo(0, 0);
+  ctx.arc(this.x, this.y, 20, Math.PI*2, 0, false);
+  ctx.stroke();
+  ctx.fill();
+  ctx.restore();
+}
