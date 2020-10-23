d593435f01a791250514bceb9e20f6e7d9c9552b
Emily Greene <798007@seq.org>
2020-10-23 11:11:04 -0700

    1023

--- Orbital Rotation/mover.js
+++ Orbital Rotation/mover.js
@@ -9,9 +9,9 @@ function Mover(x, y, dx, dy, radius, clr, numOrbs){
   this.orbiters = [];
   //create all the orbiters
   for(let i = 0; i < numOrbs; i++){
-    // let a = i * (Math.PI * 2)/numOrbs + this.orbit;
-    // let angleVel = numOrbs + 0.01;
-    // this.orbiters.push(new Orbiter(this, 4, 25, a, a));
+     let a = i * (Math.PI * 2)/numOrbs + this.orbitAngle;
+     let angleVel = numOrbs + 0.01;
+     this.orbiters.push(new Orbiter(this, 4, 25, a, angleVel, this.clr));
   }
 }
 
@@ -25,7 +25,13 @@ Mover.prototype.run = function(){
 // draw the bubble on the canvas
 Mover.prototype.render = function(){
     let ctx = game.ctx;
-    let b = game.movers;
+    let orb = game.orbiters;
+
+    for(let i = 0; i < this.orbiters.length; i++){
+      let orb = this.orbiters[i];
+      orb.update();
+      orb.render();
+    }
 
         ctx.strokeStyle = "rgba(255,255,255,255)"//this.clr;
         ctx.fillStyle = this.clr;
@@ -55,17 +61,37 @@ Mover.prototype.checkEdges = function(){
 
 
 
-  function Orbiter(){
-
-
+  function Orbiter(mover, orbiterRad, orbitRad, angle, angleVel, clr){
+    this.mover = mover;
+    this.radius = orbiterRad;
+    this.rotator = new JSVector(orbitRad, 0);
+    this.rotator.setDirection(angle);
+    this.location = JSVector.addGetNew(this.mover.location, this.rotator);
+    this.angleVel = angleVel;
+    this.clr = clr;
   }
 
   Orbiter.prototype.update = function () {
-
-
+    this.rotator.rotate(angleVel);
+    this.location = JSVector.addGetNew(this.mover.location, this.rotator);
   }
 
   Orbiter.prototype.render = function () {
-
+    let ctx = game.ctx;
+    //draw the orbiter
+    ctx.strokeStyle = this.clr;
+    ctx.fillStyle = this.clr;
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.arc(this.location.x, this.location.y, this.radius, Math.PI * 2, 0, false);
+    ctx.stroke();
+    ctx.fill();
+    //draw the line from the mover to the orbiter
+    ctx.lineCap = "round";
+    ctx.lineWidth = 4;
+    ctx.beginPath();
+    ctx.moveTo(this.mover.location.x, this.mover.location.y);
+    ctx.lineTo(this.location.x, this.location.y);
+    ctx.stroke();
 
   }
